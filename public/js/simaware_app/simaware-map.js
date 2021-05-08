8eb8add4b02e07ce4f948e264a84936f8c7fb421
Maius Wong <maiuswong@MacBook-Pro.local>
2021-05-08 13:31:43 -0700

    eurocontrol/uir fixes

--- public/js/simaware_app/simaware-map.js
+++ public/js/simaware_app/simaware-map.js
@@ -285,7 +285,8 @@ async function refreshATC()
         index = fir.fir.icao + Number(fir.fir.is_fss);
         firObj = firs_array[index];
         firname = fir.fir.name;
-        lightupFIR(firObj, fir.members, firname);
+        firicao = fir.fir.icao;
+        lightupFIR(firObj, fir.members, firname, firicao);
     })
 
     response = await fetch('/livedata/tracons.json');
@@ -313,14 +314,14 @@ async function refreshATC()
 }
 
 // Light up a FIR on the firmap
-function lightupFIR(obj, firMembers, firname)
+function lightupFIR(obj, firMembers, firname, firicao)
 {
     if(typeof obj === 'object')
     {
         $.each(obj.reverse(), function(idx, fir)
         {
             fir.setStyle({color: '#fff', weight: 1.5, fillColor: '#000', fillOpacity: 0});
-            fir.bindTooltip(getControllerBlock(obj, firMembers, firname), {sticky: true, opacity: 1});
+            fir.bindTooltip(getControllerBlock(obj, firMembers, firname, firicao), {sticky: true, opacity: 1});
             fir.bringToFront();
         });
     }
@@ -339,14 +340,14 @@ function turnOffFIR(fir)
 }
 
 // Get the controller block
-function getControllerBlock(firObj, firMembers, firname)
+function getControllerBlock(firObj, firMembers, firname, firicao)
 {
-    var list = '<table style="width: 100%; color: #333; font-size: 0.9rem"><tr><td colspan="3" style="font-size: 1rem; font-weight: 600">'+firname+'</td></tr>';
+    var list = '<table style="width: 100%; color: #333; font-size: 0.9rem"><tr><td colspan="3" style="font-size: 1rem; font-weight: 600"><span class="text-muted">'+firicao+'</span> '+firname+'</td></tr>';
     $.each(firMembers, function(idx, member) {
         if(member.fssname)
         {
-            list = list+'<tr><td style="vertical-align: middle; font-family: \'JetBrains Mono\', sans-serif; color: #9370DB">'+member.callsign+'</td><td class="px-3" style="vertical-align: middle; text-align: right; white-space: nowrap;">'+member.name+'</td><td class="text-primary" style="vertical-align: middle; font-family: \'JetBrains Mono\', monospace; letter-spacing: -0.05rem">'+member.freq+'</td><td class="ps-3 text-muted" style="vertical-align: middle; font-family: \'JetBrains Mono\', monospace; letter-spacing: -0.05rem">'+member.time_online+'</td></tr>';
-            list = list+'<tr><td colspan="4" class="small text-muted pt-0" style="line-height: 0.9rem;"><i class="fas fa-long-arrow-alt-right" style="color: #9370DB"></i> Covers '+firname+' above FL245</td></tr>';
+            list = list+'<tr><td style="vertical-align: middle; font-family: \'JetBrains Mono\', sans-serif; color: #9370DB">'+member.callsign+'<i style="display: inline; color: #9370db" class="ms-1 fas fa-angle-double-down"></i></td><td class="px-3" style="vertical-align: middle; text-align: right; white-space: nowrap;">'+member.name+'</td><td class="text-primary" style="vertical-align: middle; font-family: \'JetBrains Mono\', monospace; letter-spacing: -0.05rem">'+member.freq+'</td><td class="ps-3 text-muted" style="vertical-align: middle; font-family: \'JetBrains Mono\', monospace; letter-spacing: -0.05rem">'+member.time_online+'</td></tr>';
+            list = list+'<tr><td colspan="4" class="small text-muted pt-0" style="line-height: 0.9rem;"><b style="color: #9370db">'+member.fssname+'</b> covers '+firicao+' above FL245</td></tr>';
         }
         else
         {
