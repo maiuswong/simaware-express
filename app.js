ed882bbc30a8a35d3706d9778b145190c000b34d
Maius Wong <maiuswong@maiuss-mbp.lan>
2023-06-18 06:37:16 -0500

    modify getMarker() to use new icons

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const minify = require('express-minify');
 // Init middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Include the public folder as static
 // app.use(minify());
